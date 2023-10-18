c4c87996d3deb6d3a6f32f33716e374378642f26
Alabere-coder <alabere4real247@gmail.com>
2023-10-18 08:58:25 +0100

    adding nav, issue-page, prisma and setting it up

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,14 +1,36 @@
+"use client"
+
+import classNames from 'classnames'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 import { FaBug } from "react-icons/fa"
 
 const NavBar = () => {
+  const links = [
+    { id: 1, name: 'dashboard', href: '/' },
+    { id: 2, name: 'Issues', href: '/issues' },
+  ]
+
+  const currentPath = usePathname()
+
+
   return (
-    <nav className='flex gap-4 h-[4rem] items-center border mx-10 mt-10 justify-end'>
+    <nav className='flex h-[4rem] items-center border rounded-full mx-10 mt-10 justify-end'>
         <Link className='mx-auto text-xl' href="/"><FaBug className="h-10 w-10"/></Link>
         <ul className='flex mx-auto space-x-8 text-lg'>
-            <li><Link href="/dashboard">Dashboard</Link></li>
-            <li><Link href="/issues">Issues</Link></li>
+          {
+            links.map((item) => (
+              <Link
+              className={classNames({
+                "text-slate-400" : item.href ===currentPath,
+                "text-zinc-600" : item.href !==currentPath,
+                "hover:text-white/30 transition-colors": true
+              })}
+               href={item.href} key={item.id}>{item.name}</Link>
+            ))
+          }
+            
         </ul>
     </nav>
   )
