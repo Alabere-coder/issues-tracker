8314f0ce9e224b24c62ead36563504fbe2e99a1f
Alabere-coder <alabere4real247@gmail.com>
2023-10-21 00:58:52 +0100

    handling errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import { TextField, Button } from "@radix-ui/themes";
+import { TextField, Button, Callout } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { useState, FormEvent, ChangeEvent } from "react";
 import { useRouter } from "next/navigation";
 import { ToastContainer, toast } from "react-toastify";
+import { AiOutlineNotification } from "react-icons/ai";
 import "react-toastify/dist/ReactToastify.css";
 
 interface FormData {
@@ -14,12 +15,12 @@ interface FormData {
 }
 
 const NewIssuePage: React.FC = () => {
-
   const navigate = useRouter();
   const [formData, setFormData] = useState<FormData>({
     title: "",
     description: "",
   });
+  const [error, setError] = useState("");
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
@@ -36,41 +37,58 @@ const NewIssuePage: React.FC = () => {
         method: "POST",
         body: JSON.stringify(formData),
       });
-      const data = await response.json();
-      
-      toast("Data sent successfully");
-      navigate.push("/issues");
-      setFormData({
-        title: "",
-        description: "",
-      });
+
+    //   
+    if (response.ok) {
+        toast("Data sent successfully");
+        navigate.push("/issues");
+        setFormData({
+          title: "",
+          description: "",
+        });
+      } else {
+        const errorMessage = await response.text();
+        throw new Error(errorMessage || "An error occurred while processing the request.");
+      }
     } catch (error) {
-      console.error("Error submitting data:", error);
+      setError("Fields cannot be empty !");
     }
   };
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="flex min-h-screen space-y-4 flex-col items-center p-8"
-    >
-      <TextField.Root className="w-[60%]">
-        <TextField.Input
-          placeholder="Title"
-          name="title"
-          value={formData.title}
-          onChange={handleInputChange}
-        />
-      </TextField.Root>
+    <div className="">
+      <div className=" flex justify-center mt-4">
+      {error && (
+        <Callout.Root style={{width: 400}} color="red">
+          <Callout.Icon>
+            <AiOutlineNotification />
+          </Callout.Icon>
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      </div>
+      <form
+        onSubmit={handleSubmit}
+        className="flex min-h-screen space-y-4 flex-col items-center p-8"
+      >
+        <TextField.Root className="w-[60%]">
+          <TextField.Input
+            placeholder="Title"
+            name="title"
+            value={formData.title}
+            onChange={handleInputChange}
+          />
+        </TextField.Root>
 
-      <SimpleMDE
-        placeholder="Description"
-        onChange={(value) => setFormData({ ...formData, description: value })}
-        className="w-[60%]"
-      />
-      <Button className="w-[60%]">Add New Issue</Button>
-      <ToastContainer autoClose={false} />
-    </form>
+        <SimpleMDE
+          placeholder="Description"
+          onChange={(value) => setFormData({ ...formData, description: value })}
+          className="w-[60%]"
+        />
+        <Button className="w-[60%]">Add New Issue</Button>
+        <ToastContainer autoClose={false} />
+      </form>
+    </div>
   );
 };
 
